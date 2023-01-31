285d223761ee258dce9dfa73d62ab41baf6221f5
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-01-31 17:02:06 +0700

    Java Solution

--- Array/Two_Sum.java
+++ Array/Two_Sum.java
@@ -1,19 +1,38 @@
 package Array;
 
+//class Solution_Two_Sum {
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] returnAns = new int[2];
+//        for(int i = 0; i < nums.length - 1; i++) {
+//            for(int j = i+1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    returnAns[0] = i;
+//                    returnAns[1] = j;
+//                }
+//            }
+//        }
+//        return returnAns;
+//    }
+//}
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution_Two_Sum {
     public int[] twoSum(int[] nums, int target) {
-        int[] returnAns = new int[2];
-        for(int i = 0; i < nums.length - 1; i++) {
-            for(int j = i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    returnAns[0] = i;
-                    returnAns[1] = j;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+            map.put(nums[i], i);
         }
-        return returnAns;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
+
 
 public class Two_Sum {
     public static void main(String[] args) {
