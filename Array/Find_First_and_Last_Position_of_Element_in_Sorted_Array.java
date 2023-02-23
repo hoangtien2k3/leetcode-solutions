8c458da16f80477ca46115df7550775070bd76c1
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-02-23 21:33:06 +0700

    LeetCode Solution

--- Array/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ Array/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -1,38 +1,95 @@
 package Array;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 class Solution_Find_First_and_Last_Position_of_Element_in_Sorted_Array {
+
+    // cách 1:
+//    public int[] searchRange(int[] nums, int target) {
+//        int start = binarySearch(nums, target);
+//        if (start == nums.length || nums[start] != target) {
+//            return new int[]{-1, -1};
+//        }
+//        return new int[]{start, binarySearch(nums, target + 1) - 1};
+//    }
+//
+//    public int binarySearch(int[] nums, int target) {
+//        int l = 0, r = nums.length ;
+//        while(l < r) {
+//            int m = l + ((r - l) >> 1);
+//            if (nums[m] > target || nums[m] == target) {
+//                r = m; // không nên cho r = m - 1 khi mà nums[m] == target
+//            } else {
+//                l = m + 1;
+//            }
+//        }
+//        return l;
+//    }
+
+
+
+
+    // cach 2:
+
     public int[] searchRange(int[] nums, int target) {
-        int start = binarySearch(nums, target);
-        if (start == nums.length || nums[start] != target) {
-            return new int[]{-1, -1};
-        }
-        return new int[]{start, binarySearch(nums, target + 1) - 1};
+        int[] result = new int[2];
+        result[0] = findStartingIndex(nums, target);
+        result[1] = findEndingIndex(nums, target);
+        return result;
     }
 
-    public int binarySearch(int[] nums, int target) {
-        int l = 0, r = nums.length ;
-        while(l < r) {
-            int m = l + ((r - l) >> 1);
-            if (nums[m] > target || nums[m] == target) {
-                r = m; // không nên cho r = m - 1 khi mà nums[m] == target
+
+    public int findStartingIndex(int[] nums, int target) {
+        int index = -1;
+
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end) {
+            int mid = start + ((end - start) >> 1);
+            if (nums[mid] >= target) {
+                end = mid-1;
             } else {
-                l = m + 1;
+                start = mid+1;
             }
+            if (nums[mid] == target) index = mid;
         }
-        return l;
+
+        return index;
     }
+
+    public int findEndingIndex(int[] nums, int target) {
+        int index = -1;
+
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end) {
+            int mid = start + ((end - start) >> 1);
+            if (nums[mid] <= target) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+
+        return index;
+    }
+
 }
 
 public class Find_First_and_Last_Position_of_Element_in_Sorted_Array {
     public static void main(String[] args) {
-        int[] nums = new int[]{5,7,7,8,8,10};
+        int[] nums = new int[]{5, 7, 7, 8, 8, 8, 10};
         int target = 8;
 
         Solution_Find_First_and_Last_Position_of_Element_in_Sorted_Array solution = new Solution_Find_First_and_Last_Position_of_Element_in_Sorted_Array();
         int[] ans = solution.searchRange(nums, target);
-        System.out.println(ans);
+
+        List<Integer> list = new ArrayList<>();
+        list = Arrays.asList(ans[0], ans[1]);
+        System.out.println(list);
     }
 }
