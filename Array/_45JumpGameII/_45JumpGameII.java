a2a81c764b57b0f29ea8eb8b263d13ad7e7f66e8
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-04-15 10:44:01 +0700

    update

---
+++ Array/_45JumpGameII/_45JumpGameII.java
@@ -0,0 +1,29 @@
+package Array._45JumpGameII;
+
+class Solution {
+    public int jump(int[] nums) {
+        int jump_Index = 0, l = 0, r = 0;
+
+        while(r < nums.length - 1) {
+            int check = 0;
+            for(int i = l; i <= r; i++) {
+                check = Math.max(check, nums[i] + i);
+            }
+            l = r + 1;
+            r = check;
+            ++jump_Index;
+        }
+
+        return jump_Index;
+    }
+}
+
+public class _45JumpGameII {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = new int[] {1,2,3,4,5};
+        System.out.println(solution.jump(nums));
+
+    }
+}
