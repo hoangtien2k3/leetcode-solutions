f22ca56dea43e96514d23b97a46bc60d3a0d0d39
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-07-16 22:00:00 +0530

    update

--- String/_415AddStrings/_415AddStrings.java
+++ String/_415AddStrings/_415AddStrings.java
@@ -4,8 +4,12 @@ class Solution {
     public static String addStrings(String num1, String num2) {
         String res = "";
 
-        while(num1.length() > num2.length()) num2 = "0" + num2;
-        while(num1.length() < num2.length()) num1 = "0" + num1;
+        StringBuilder num2Builder = new StringBuilder(num2);
+        while(num1.length() > num2Builder.length()) num2Builder.insert(0, "0");
+        num2 = num2Builder.toString();
+        StringBuilder num1Builder = new StringBuilder(num1);
+        while(num1Builder.length() < num2.length()) num1Builder.insert(0, "0");
+        num1 = num1Builder.toString();
 
         int carry = 0;
         for(int i = num1.length() - 1; i >= 0; i--) {
