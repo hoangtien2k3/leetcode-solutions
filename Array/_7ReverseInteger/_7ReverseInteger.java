4ad9be0924ed7f005b5ec0341294b8eb207016d8
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-07-31 22:00:00 +0530

    update

--- Array/_7ReverseInteger/_7ReverseInteger.java
+++ Array/_7ReverseInteger/_7ReverseInteger.java
@@ -2,7 +2,6 @@ package Array._7ReverseInteger;
 
 class Solution {
     public static int reverse(int x) {
-
         String xx = Integer.toString(x);
         String check = null;
         if (x < 0) {
@@ -14,16 +13,12 @@ class Solution {
         StringBuilder stringBuilder = new StringBuilder(check);
         stringBuilder.reverse();
 
-        if (x < 0) {
-            stringBuilder.insert(0, '-');
-        }
+        if (x < 0) stringBuilder.insert(0, '-');
 
         long value = Long.parseLong(stringBuilder.toString());
-        if (value > Integer.MAX_VALUE || value < Integer.MIN_VALUE) {
-            return 0;
-        }
+        if (value > Integer.MAX_VALUE || value < Integer.MIN_VALUE) return 0;
 
-        return (int)value;
+        return (int) value;
     }
 }
 
