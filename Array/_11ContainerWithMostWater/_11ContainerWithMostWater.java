f9dbe6356ed9ab72e6a5134c8f2dc6542633c349
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-08-25 22:00:00 +0530

    update

--- Array/_11ContainerWithMostWater/_11ContainerWithMostWater.java
+++ Array/_11ContainerWithMostWater/_11ContainerWithMostWater.java
@@ -5,9 +5,10 @@ class Solution {
         // kỹ thuật dùng dịch 2 con trỏ.
         int a_pointer = 0;
         int b_pointer = height.length - 1;
+
         int max_area = 0;
-        while(a_pointer < b_pointer) {
-            if(height[a_pointer] < height[b_pointer]) {
+        while (a_pointer < b_pointer) {
+            if (height[a_pointer] < height[b_pointer]) {
                 max_area = Math.max(max_area, height[a_pointer] * (b_pointer - a_pointer));
                 a_pointer++;
             } else {
@@ -22,7 +23,7 @@ class Solution {
 
 public class _11ContainerWithMostWater {
     public static void main(String[] args) {
-        int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+        int[] height = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
         Solution solution = new Solution();
         System.out.println(solution.maxArea(height));
     }
