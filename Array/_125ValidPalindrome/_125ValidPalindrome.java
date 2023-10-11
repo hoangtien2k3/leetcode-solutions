95a6e0c071fd88cf4bcb789ca34146dc40fd5883
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-10-11 22:00:00 +0530

    update

--- Array/_125ValidPalindrome/_125ValidPalindrome.java
+++ Array/_125ValidPalindrome/_125ValidPalindrome.java
@@ -3,9 +3,8 @@ package Array._125ValidPalindrome;
 class Solution {
     public boolean isPalindrome(String s) {
         // vì bài này có các ý tự không phải chữ số hay chữ cái. nên ko dùng reverst() được
-
         int left = 0, right = s.length() - 1;
-        while(left < right) {
+        while (left < right) {
             // Character.isLetterOrDigit(s.charAt(left): kiểm tra ký tự đó có phải chữ cái hay chữ số không, nếu không thì trả về false.
             if (!Character.isLetterOrDigit(s.charAt(left))) {
                 ++left;
@@ -25,7 +24,6 @@ class Solution {
 public class _125ValidPalindrome {
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-
         Solution solution = new Solution();
         System.out.println(solution.isPalindrome(s));
     }
