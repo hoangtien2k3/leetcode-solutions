430f3f5ca9ba28a1bd1c8be81f3608f313f3ca64
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-10-12 22:00:00 +0530

    update

--- Array/_128LongestConsecutiveSequence/_128LongestConsecutiveSequence.java
+++ Array/_128LongestConsecutiveSequence/_128LongestConsecutiveSequence.java
@@ -4,14 +4,11 @@ import java.util.Arrays;
 
 class Solution {
     public static int longestConsecutive(int[] nums) {
-        if (nums.length == 0)
-            return 0;
-
+        if (nums.length == 0) return 0;
         Arrays.sort(nums);
 
         int count = 1;
         int checkCount = 1;
-
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
                 if (nums[i] == nums[i - 1] + 1) {
@@ -22,7 +19,6 @@ class Solution {
                 }
             }
         }
-
         return Math.max(count, checkCount);
     }
 }
