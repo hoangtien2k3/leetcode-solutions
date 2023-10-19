aa5370ade0527c97a951255bc786b6725cd6af2c
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-10-19 22:00:00 +0530

    update

--- Array/_1146SnapshotArray/_1146SnapshotArray.java
+++ Array/_1146SnapshotArray/_1146SnapshotArray.java
@@ -35,20 +35,12 @@ class SnapshotArray {
     }
 }
 
-/**
- * Your SnapshotArray object will be instantiated and called as such:
- * SnapshotArray obj = new SnapshotArray(length);
- * obj.set(index,val);
- * int param_2 = obj.snap();
- * int param_3 = obj.get(index,snap_id);
- */
-
 public class _1146SnapshotArray {
     public static void main(String[] args) {
         SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
-        snapshotArr.set(0,5);  // Set array[0] = 5
+        snapshotArr.set(0, 5);  // Set array[0] = 5
         snapshotArr.snap();  // Take a snapshot, return snap_id = 0
-        snapshotArr.set(0,6);
-        snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
+        snapshotArr.set(0, 6);
+        snapshotArr.get(0, 0);  // Get the value of array[0] with snap_id = 0, return 5
     }
 }
